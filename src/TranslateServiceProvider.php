88989551ab0ef5fc1a6af5870edfc398d5acbd8f
Faros <114036707+a-faros@users.noreply.github.com>
2026-09-25 15:05:49 +0800

    Release v1.4.3

--- src/TranslateServiceProvider.php
+++ src/TranslateServiceProvider.php
@@ -6,7 +6,6 @@ namespace Rimba\Translate;
 
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
-use Illuminate\Contracts\View\Factory;
 use Rimba\Base\Services\BitesServiceProvider;
 use Rimba\Translate\Enums\Language;
 
@@ -21,11 +20,11 @@ class TranslateServiceProvider extends BitesServiceProvider
         $languages = Language::googleTranslateLanguages();
         FilamentView::registerRenderHook(
             PanelsRenderHook::USER_MENU_AFTER,
-            fn (): Factory|\Illuminate\Contracts\View\View => view('bites::language-switch')
+            fn () => view('bites::language-switch')
         );
         FilamentView::registerRenderHook(
             PanelsRenderHook::SIMPLE_PAGE_END,
-            fn (): Factory|\Illuminate\Contracts\View\View => view('bites::language-switch')
+            fn () => view('bites::language-switch')
         );
         FilamentView::registerRenderHook(
             PanelsRenderHook::FOOTER,
